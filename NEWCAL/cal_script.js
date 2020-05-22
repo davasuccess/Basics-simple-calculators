fd148e0bba6142ddf59a08a25abf2df841b615da
David Aina <davasuccess20000@gmail.com>
2020-05-22 12:52:47 +0100

    uploading the calculator folder

---
+++ NEWCAL/cal_script.js
@@ -0,0 +1,24 @@
+alert("welcome")
+
+var backspace = function(){
+     var carrier =document.getElementById("output").value;
+    document.getElementById("output").value=carrier.substring(0,carrier.length -1 );
+}
+
+ function factoria(fac){
+    //var fac =" "
+
+    if(fac === 0){
+        return 1;
+    }
+    
+    return fac * factoria(fac-1);
+    
+}var pacentage=  function(){
+    var parate =cal.output.value;
+    var pavalue = cal.output.value 
+}
+    var rate =parate / 100
+    var pa = rate *pavalue
+    
+
---
+++ javascript.js
@@ -0,0 +1,19 @@
+var exp= " ", equal, number, operstor, decimal, allowSH = true;
+var entervalue = document.forms['myform']['entervalue'];
+alert('Welcome');
+
+function insertNUm(num){
+if(equel){
+    exp = num;
+    entervalue.value= exp;
+    number=true; 
+    equal=false;
+}
+else{
+    exp= entervalue.value + num;
+entervalue.value=exp;
+number=true;
+    }
+    if(operator) decimal=false;
+    //sr('a')
+}
